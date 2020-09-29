85498ff2dd775f33c7b3bea35178d365fdea0974
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-29 18:49:50 +0800

    >  compile NEMU 3019244354 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  18:49:50 up 17 min,  1 user,  load average: 0.63, 0.36, 0.29 59eba94b75d3acdad13fa9cca49595343b3e4da4

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -43,8 +43,8 @@ static int cmd_si(char *args){
 	int si_cnt = 0;
 	int i;	
 	for(i = 0;i < si_tmp;i++){
-		si_cnt += (args[i]-'0');
-		if(i!=si_tmp-1)si_cnt = si_cnt * 10;
+		si_cnt = si_cnt + (args[i]-'0');
+		if(i!=si_tmp-1){si_cnt = si_cnt * 10;}
 	}
 	cpu_exec(si_cnt);
 	return 0;
