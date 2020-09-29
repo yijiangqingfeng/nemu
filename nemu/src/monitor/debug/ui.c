2ac02621eb7ff47340974e62e0e20b6d1a7d67d2
tracer-NEMU2020 <tjuics2020@163.com>
2020-09-29 17:21:00 +0800

    >  compile NEMU 3019244354 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  17:21:00 up 22:47,  1 user,  load average: 0.54, 0.50, 0.49 f054a84cb9cbd497822fb05cb17774e38ec1759

--- nemu/src/monitor/debug/ui.c
+++ nemu/src/monitor/debug/ui.c
@@ -37,9 +37,16 @@ static int cmd_q(char *args) {
 }
 
 static int cmd_help(char *args);
-
+static int cmd_si(char *args);
 static int cmd_si(char *args){
-	return 0;
+	int si_tmp = sizeof(args);
+	int si_cnt = 0;
+	int i;	
+	for(i = 0;i < si_tmp;i++){
+		si_cnt += (args[i]-'0');
+		si_cnt*=10;
+	}
+	return si_cnt;
 }
 
 static struct {
