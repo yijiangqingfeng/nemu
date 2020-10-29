377199b611b31dd9bee09bb44d1b821f6cdca73e
tracer-ics2016 <tracer@njuics.org>
2020-10-29 20:26:34 +0800

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-118-generic #119-Ubuntu SMP Tue Sep 8 12:30:01 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:26:34 up 17 days,  9:29,  1 user,  load average: 0.08, 0.29, 0.28 a9accbc1775227b2dd22e3370406dffef7ed4eb

--- nemu/src/cpu/exec/arith/je-template.h
+++ nemu/src/cpu/exec/arith/je-template.h
@@ -5,6 +5,7 @@
 make_helper(concat(je_i_,SUFFIX)){
     int len = concat(decode_i_,SUFFIX)(cpu.eip + 1);
     if(cpu.ZF==1)cpu.eip+=op_src -> val;
+    cpu.eip += 1;
     print_asm("je\t%x",cpu.eip+1);
     return len + 1;
 }
