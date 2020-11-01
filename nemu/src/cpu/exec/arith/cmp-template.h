4947dc45b4d697186cf0e28299940ac66b95d81a
tracer-ics2016 <tracer@njuics.org>
2020-11-01 19:42:43 +0800

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:42:43 up 23:42,  1 user,  load average: 1.03, 0.53, 0.36 5f20646961ed455ccdc44f95b50e9886461aba5

--- nemu/src/cpu/exec/arith/cmp-template.h
+++ nemu/src/cpu/exec/arith/cmp-template.h
@@ -3,6 +3,19 @@
 #define instr cmp
 
 static void do_execute(){
-	
+	if(op_src -> val > op_dest -> val){
+		cpu.ZF=0;
+		cpu.CF=1;
+	}else if(op_src -> val == op_dest -> val){
+		cpu.ZF=1;
+		cpu.CF=0;
+	}else{
+		cpu.ZF=0;
+		cpu.CF=0;
+	}
+	print_asm_template2();
 }
+
+make_instr_helper(i2rm)
+
 #include "cpu/exec/template-end.h"
