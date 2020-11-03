e61565240529605821f8d0331f1b6a7601f81379
tracer-ics2016 <tracer@njuics.org>
2020-11-03 21:48:57 +0800

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  21:48:57 up 3 days,  1:48,  1 user,  load average: 0.72, 0.58, 0.61 7ced4749a7303cbf8109b9cd12a92383758eed80

--- nemu/src/cpu/exec/exec.c
+++ nemu/src/cpu/exec/exec.c
@@ -237,5 +237,5 @@ static make_helper(_2byte_esc) {
 	//cpu.eip ++;
 	uint32_t opcode = instr_fetch(eip, 1);
 	ops_decoded.opcode = opcode | 0x100;
-	return _2byte_opcode_table[opcode](eip); 
+	return _2byte_opcode_table[opcode](eip)+1; 
 }
