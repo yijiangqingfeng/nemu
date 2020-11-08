218be47841347b076297038bcd001c3e62e4dfdf
tracer-ics2016 <tracer@njuics.org>
2020-11-08 11:41:25 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:41:25 up  5:23,  1 user,  load average: 0.90, 0.42, 0.32 426770e4a97e04db7864fd3c1b186d9cba0427

--- nemu/src/cpu/exec/string/scas-template.h
+++ nemu/src/cpu/exec/string/scas-template.h
@@ -4,14 +4,39 @@
 
 static void do_execute(){
 	if(ops_decoded.is_stack_size_16){
-		swaddr_write(reg_w(R_AX),2,swaddr_read(reg_w(R_DI),4));
+		DATA_TYPE rst = reg_w(R_AX)-reg_w(R_DI);
+		int len = (DATA_BYTE << 3) -1;
+		cpu.CF = op_dest -> val < op_src -> val;
+		cpu.SF = rst >> len;
+		int s1,s2;
+		s1 = op_dest -> val >> len;
+		s2 = op_src -> val >> len;
+		cpu.OF = (s1!=s2 && s2 == cpu.SF);
+		cpu.ZF = !rst;
+		rst ^= rst >> 4;
+		rst ^= rst >> 2;
+		rst ^= rst >> 1;
+		cpu.PF = !(rst & 1);
 		if(cpu.DF==0){
 			reg_w(R_DI)+=DATA_BYTE;
 		}else{
 			reg_w(R_DI)-=DATA_BYTE;
 		}
 	}else{
-		swaddr_write(reg_l(R_EAX),4,swaddr_read(reg_l(R_EDI),4));
+		DATA_TYPE rst = reg_w(R_AX)-reg_w(R_DI);
+		//swaddr_write(reg_l(R_EAX),4,swaddr_read(reg_l(R_EDI),4));
+		int len = (DATA_BYTE << 3) -1;
+		cpu.CF = op_dest -> val < op_src -> val;
+		cpu.SF = rst >> len;
+		int s1,s2;
+		s1 = op_dest -> val >> len;
+		s2 = op_src -> val >> len;
+		cpu.OF = (s1!=s2 && s2 == cpu.SF);
+		cpu.ZF = !rst;
+		rst ^= rst >> 4;
+		rst ^= rst >> 2;
+		rst ^= rst >> 1;
+		cpu.PF = !(rst & 1);
 		if(cpu.DF==0){
 			reg_l(R_EDI)+=DATA_BYTE;
 		}else{
