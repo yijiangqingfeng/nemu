dc9b57259881126772157b685c83d34385e45c43
tracer-ics2016 <tracer@njuics.org>
2020-11-09 15:13:36 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:13:36 up 1 day,  8:56,  1 user,  load average: 0.35, 0.54, 0.57 f8784ef5b9efa13d8590a071724b574210412c7e

--- nemu/src/cpu/exec/arith/jmp-template.h
+++ nemu/src/cpu/exec/arith/jmp-template.h
@@ -4,14 +4,15 @@
 
 static void do_execute(){
 	DATA_TYPE_S imm = op_src -> val;
-	if(op_src -> type == OP_TYPE_REG || op_src -> type == OP_TYPE_MEM)
+	if(op_src -> type == OP_TYPE_IMM)
 	{
-		cpu.eip = imm - concat ( decode_rm_,SUFFIX)(cpu.eip+1)-1;
+		cpu.eip += imm ;
 		print_asm_no_template1();
 	}else{
-		print_asm("jmp\t%x",cpu.eip+1+DATA_BYTE+imm);
-		cpu.eip+=imm;
+		int len = concat(decode_rm_,SUFFIX)(cpu.eip+1);
+		cpu.eip = imm - len - 1;
 	}
+	print_asm_template1();
 }
 make_instr_helper(i)
 make_instr_helper(rm)
