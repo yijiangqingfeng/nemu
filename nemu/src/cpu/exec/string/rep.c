f23eeec66448346da717593e6fe144973f78756a
tracer-ics2016 <tracer@njuics.org>
2020-11-09 09:57:08 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  09:57:08 up 1 day,  3:39,  1 user,  load average: 0.43, 0.40, 0.40 5d556c3a77620c56a59e7501130faf15d97cf008

--- nemu/src/cpu/exec/string/rep.c
+++ nemu/src/cpu/exec/string/rep.c
@@ -26,7 +26,7 @@ make_helper(rep) {
 				);
 
 			/* TODO: Jump out of the while loop if necessary. */
-			if((ops_decoded.opcode == 0xa6||ops_decoded.opcode == 0xa7|| ops_decoded.opcode == 0xae|| ops_decoded.opcode == 0xaf)&&cpu.ZF==0)break;
+			if((ops_decoded.opcode == 0xa6||ops_decoded.opcode == 0xa7|| ops_decoded.opcode == 0xae|| ops_decoded.opcode == 0xaf)&&cpu.ZF==1)break;
 		}
 	}
 	len = 1;
@@ -51,7 +51,7 @@ make_helper(repnz) {
 				|| ops_decoded.opcode == 0xae	// scasb
 				|| ops_decoded.opcode == 0xaf	// scasw
 			  );
-		if(cpu.ZF==0)break;
+		if((ops_decoded.opcode == 0xa6||ops_decoded.opcode == 0xa7|| ops_decoded.opcode == 0xae|| ops_decoded.opcode == 0xaf)&&cpu.ZF==0)break;
 		/* TODO: Jump out of the while loop if necessary. */
 
 	}
