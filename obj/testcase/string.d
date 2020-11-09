560f38804558292359719c449a1b3889f2caf384
tracer-ics2016 <tracer@njuics.org>
2020-11-09 10:13:59 +0000

    >  run 151220000 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  10:13:59 up 1 day,  3:56,  1 user,  load average: 1.42, 0.78, 0.51 3b415b20bebfad9ef4e7fc588a126975379d15e5

---
+++ obj/testcase/string.d
@@ -0,0 +1,4 @@
+obj/testcase/string.o: testcase/src/string.c lib-common/trap.h \
+ lib-common/uclibc/include/string.h lib-common/uclibc/include/features.h \
+ lib-common/uclibc/include/bits/uClibc_config.h \
+ lib-common/uclibc/include/sys/cdefs.h
