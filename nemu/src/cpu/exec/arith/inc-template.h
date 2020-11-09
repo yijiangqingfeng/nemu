dbf1807c8a3b829dc46d4ff4120f0ed8101a2b92
tracer-ics2016 <tracer@njuics.org>
2020-11-09 11:20:17 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:20:17 up 1 day,  5:02,  1 user,  load average: 0.63, 0.44, 0.45 61ba3823be73642e3e1c23fb5e75e68eaae903

--- nemu/src/cpu/exec/arith/inc-template.h
+++ nemu/src/cpu/exec/arith/inc-template.h
@@ -11,7 +11,7 @@ static void do_execute () {
 	cpu.ZF=!result;
 	int s1=0;
 	//int s2=op_src->val>>len;
-	int s2 = MSB(op_src->val);
+	int s2 = op_src->val >> len;
 	cpu.OF=(s1==s2)&&(cpu.SF != s2);
 	result^=result >>4;
 	result^=result >>2;
