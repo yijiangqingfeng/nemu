288cde4cfef633c7e9f0037c1512e970d8bc954c
tracer-ics2016 <tracer@njuics.org>
2020-11-09 07:41:44 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  07:41:44 up 1 day,  1:24,  1 user,  load average: 0.34, 0.33, 0.16 e6753ae56fa15193b33f8e22891f2181b363809b

--- nemu/src/cpu/exec/string/lods-template.h
+++ nemu/src/cpu/exec/string/lods-template.h
@@ -1,24 +1,14 @@
 #include "cpu/exec/template-start.h"
 
 #define instr lods
-
-static void do_execute(){
-	if(ops_decoded.is_stack_size_16){
-		reg_w(R_AL) = swaddr_read(reg_l(R_SI),4);
-		if(cpu.DF==0){
-			reg_w(R_SI)+=DATA_BYTE;
-		}else{
-			reg_w(R_SI)-=DATA_BYTE;
-		}
+make_helper(concat(lods_n_,SUFFIX)){
+	REG(R_EAX) = swaddr_read(reg_l(R_ESI),DATA_BYTE);
+	if(cpu.DF==0){
+		reg_l(R_ESI) += DATA_BYTE;
 	}else{
-		reg_l(R_AL) = swaddr_read(reg_l(R_ESI),4);
-		if(cpu.DF==0){
-			reg_l(R_ESI)+=DATA_BYTE;
-		}else{
-			reg_l(R_ESI)-=DATA_BYTE;
-		}
+		reg_l(R_ESI) -= DATA_BYTE;
 	}
 	print_asm("lods");
+	return 1;
 }
-make_instr_helper(n)
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/string/scas-template.h
+++ nemu/src/cpu/exec/string/scas-template.h
@@ -3,47 +3,24 @@
 #define instr scas
 
 static void do_execute(){
-	if(ops_decoded.is_stack_size_16){
-		DATA_TYPE rst = reg_w(R_AL)-swaddr_read(reg_l(R_EDI),4);
-		//int len = (DATA_BYTE << 3) -1;
-		//cpu.CF = op_dest -> val < op_src -> val;
-		//cpu.SF = rst >> len;
-		//int s1,s2;
-		//s1 = op_dest -> val >> len;
-		//s2 = op_src -> val >> len;
-		//cpu.OF = (s1!=s2 && s2 == cpu.SF);
-		cpu.ZF = !rst;
-		rst ^= rst >> 4;
-		rst ^= rst >> 2;
-		rst ^= rst >> 1;
-		cpu.PF = !(rst & 1);
-		if(cpu.DF==0){
-			reg_w(R_DI)+=DATA_BYTE;
-		}else{
-			reg_w(R_DI)-=DATA_BYTE;
-		}
-	}else{
-		DATA_TYPE rst = reg_l(R_AL)-swaddr_read(reg_l(R_EDI),4);
-		//swaddr_write(reg_l(R_EAX),4,swaddr_read(reg_l(R_EDI),4));
-		//int len = (DATA_BYTE << 3) -1;
-		//cpu.CF = reg_l(R_EAX)<reg_l(R_EDI);
-		//cpu.SF = rst >> len;
-		//int s1,s2;
-		//s1 = op_dest -> val >> len;
-		//s2 = op_src -> val >> len;
-		//cpu.OF = (s1!=s2 && s2 == cpu.SF);
-		cpu.ZF = !rst;
-		rst ^= rst >> 4;
-		rst ^= rst >> 2;
-		rst ^= rst >> 1;
-		cpu.PF = !(rst & 1);
-		if(cpu.DF==0){
-			reg_l(R_EDI)+=DATA_BYTE;
-		}else{
-			reg_l(R_EDI)-=DATA_BYTE;
-		}
-	}
+	DATA_TYPE dest = REG(R_EAX);
+	DATA_TYPE src = swaddr_read(reg_l(R_EDI),DATA_BYTE);
+	DATA_TYPE rst = dest - src ;
+	if(cpu.DF==0)reg_l(R_EDI) += DATA_BYTE;
+	else reg_l(R_EDI) -= DATA_BYTE;
 	print_asm("scas");
+	if(dest<src)cpu.CF=1;
+	int a = op_dest -> val >> (DATA_BYTE*8-1);
+	int b = op_src -> val >> (DATA_BYTE*8-1);
+	cpu.OF=(a!=b)&&(b==cpu.SF);
+	cpu.ZF=!rst;
+	int s = 0;
+	int i;
+	for(i = 0;i < 8;i++)s+=(rst>>i)&1;
+	if(s % 2)cpu.PF = 0;
+	else cpu.PF = 1;
+	if(((op_dest->val&0xf)-(op_src->val &0xf))>>4)cpu.AF=1;
+	else cpu.ZF=0;
 }
 make_instr_helper(n)
 #include "cpu/exec/template-end.h"
