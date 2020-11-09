797f00571691a1d2ae700cadc0dd520f4fe02a44
tracer-ics2016 <tracer@njuics.org>
2020-11-09 15:01:40 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  15:01:40 up 1 day,  8:44,  1 user,  load average: 0.92, 0.70, 0.60 4f8d787d7463cbc8e2077c077baefe99f463a46

--- nemu/src/cpu/exec/arith/pop-template.h
+++ nemu/src/cpu/exec/arith/pop-template.h
@@ -17,8 +17,7 @@ static void do_execute(){
 	}*/
 	print_asm_no_template1();
 }
-#if DATA_BYTE == 2 || DATA_BYTE == 4
 make_instr_helper(r)
-#endif
+make_instr_helper(rm)
 
 #include "cpu/exec/template-end.h"
--- nemu/src/cpu/exec/arith/pop.c
+++ nemu/src/cpu/exec/arith/pop.c
@@ -9,3 +9,4 @@
 #undef DATA_BYTE 
 
 make_helper_v(pop_r)
+make_helper_v(pop_rm)
