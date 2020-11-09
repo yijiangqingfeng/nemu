43c9eaaa0f6b5c2459330d726a35a469ec7427a1
tracer-ics2016 <tracer@njuics.org>
2020-11-09 11:29:02 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  11:29:02 up 1 day,  5:11,  1 user,  load average: 0.18, 0.31, 0.39 3e9f8984c28bc7747afa346b3a719439a8eb03c

--- nemu/src/cpu/exec/arith/push-template.h
+++ nemu/src/cpu/exec/arith/push-template.h
@@ -3,9 +3,14 @@
 #define instr push
 
 static void do_execute(){
-	if(DATA_BYTE == 1)op_src->val = (int8_t)op_src->val;
-	reg_l (R_ESP) -= 4;
-	swaddr_write (reg_l (R_ESP) , 4 ,op_src->val);
+	if(DATA_BYTE == 2){
+		reg_l (R_ESP) -= 2;
+		MEM_W(reg_l(R_ESP) ,op_src->val);
+	}else{
+		reg_l (R_ESP) -= 4;
+		if(DATA_BYTE == 1)op_src->val = (uint8_t) op_src->val;
+	swaddr_write(reg_l(R_ESP),4,op_src->val);
+	}
 	print_asm_no_template1();
 }
 
