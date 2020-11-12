1bde48358ad0ea61933030d88eb6ab7cf26c9833
tracer-ics2016 <tracer@njuics.org>
2020-11-12 12:40:30 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  12:40:30 up 2 days, 20:26,  1 user,  load average: 0.82, 0.27, 0.13 4d8b4274e4cc83ef597afdb7201c107978fac1

--- nemu/src/monitor/debug/elf.c
+++ nemu/src/monitor/debug/elf.c
@@ -16,6 +16,7 @@ uint32_t getValue(char* str,bool* success){
 			int tmp_len = strlen(str);
 			strncpy(a,strtab + symtab[tmp2].st_name,tmp_len);
 			a[tmp_len] = '\0';
+			printf("\n%s\n",a);
 			if(strcmp(a,str)==0)return symtab[tmp2].st_value;
 		}
 	}
