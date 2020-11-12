9f8da51c22fd9718343c84c4bcb4813a3a2462ad
tracer-ics2016 <tracer@njuics.org>
2020-11-12 16:14:19 +0000

    >  test 151220000 simpleedu Linux simpleedu 4.15.0-112-generic #113-Ubuntu SMP Thu Jul 9 23:41:39 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  16:14:19 up 3 days, 0 min,  1 user,  load average: 0.75, 0.56, 0.37 868a598b8200f2c46e9367947b24b0e5b3c986

---
+++ obj/testcase/print-FLOAT.d
@@ -0,0 +1,22 @@
+obj/testcase/print-FLOAT.o: testcase/src/print-FLOAT.c \
+ lib-common/uclibc/include/stdio.h lib-common/uclibc/include/features.h \
+ lib-common/uclibc/include/bits/uClibc_config.h \
+ lib-common/uclibc/include/sys/cdefs.h \
+ lib-common/uclibc/include/bits/types.h \
+ lib-common/uclibc/include/bits/wordsize.h \
+ lib-common/uclibc/include/bits/typesizes.h \
+ lib-common/uclibc/include/bits/pthreadtypes.h \
+ lib-common/uclibc/include/bits/sched.h \
+ lib-common/uclibc/include/bits/uClibc_stdio.h \
+ lib-common/uclibc/include/bits/uClibc_mutex.h \
+ lib-common/uclibc/include/pthread.h lib-common/uclibc/include/sched.h \
+ lib-common/uclibc/include/time.h lib-common/uclibc/include/bits/time.h \
+ lib-common/uclibc/include/bits/uClibc_clk_tck.h \
+ lib-common/uclibc/include/signal.h \
+ lib-common/uclibc/include/bits/sigset.h \
+ lib-common/uclibc/include/bits/initspin.h \
+ lib-common/uclibc/include/bits/sigthread.h \
+ lib-common/uclibc/include/bits/uClibc_pthread.h \
+ lib-common/uclibc/include/bits/stdio_lim.h \
+ lib-common/uclibc/include/string.h lib-common/FLOAT.h lib-common/trap.h \
+ lib-common/trap.h
