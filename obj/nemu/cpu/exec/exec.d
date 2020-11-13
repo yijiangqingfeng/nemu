7047b61f8a803c9d378428f77d9ae0908bb9b212
tracer-ics2016 <tracer@njuics.org>
2020-11-13 02:28:54 +0000

    >  compile NEMU 151220000 simpleedu Linux simpleedu 4.15.0-122-generic #124-Ubuntu SMP Thu Oct 15 13:03:05 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  02:28:54 up  9:56,  1 user,  load average: 0.70, 0.23, 0.07 5c82618ae01a33dab7923a5d963810765427a5fe

---
+++ obj/nemu/cpu/exec/exec.d
@@ -0,0 +1,34 @@
+obj/nemu/cpu/exec/exec.o: nemu/src/cpu/exec/exec.c \
+ nemu/include/cpu/helper.h nemu/include/nemu.h nemu/include/common.h \
+ nemu/include/debug.h nemu/include/macro.h nemu/include/memory/memory.h \
+ nemu/include/common.h nemu/include/cpu/reg.h \
+ nemu/include/cpu/decode/operand.h nemu/include/cpu/decode/modrm.h \
+ nemu/src/cpu/exec/all-instr.h nemu/src/cpu/exec/prefix/prefix.h \
+ nemu/src/cpu/exec/data-mov/mov.h nemu/src/cpu/exec/data-mov/xchg.h \
+ nemu/src/cpu/exec/data-mov/movext.h nemu/src/cpu/exec/data-mov/movsx.h \
+ nemu/src/cpu/exec/data-mov/movzb.h nemu/src/cpu/exec/data-mov/movzw.h \
+ nemu/src/cpu/exec/data-mov/cltd.h nemu/src/cpu/exec/data-mov/leave.h \
+ nemu/src/cpu/exec/data-mov/call.h nemu/src/cpu/exec/arith/dec.h \
+ nemu/src/cpu/exec/arith/inc.h nemu/src/cpu/exec/arith/neg.h \
+ nemu/src/cpu/exec/arith/imul.h nemu/src/cpu/exec/arith/mul.h \
+ nemu/src/cpu/exec/arith/idiv.h nemu/src/cpu/exec/arith/div.h \
+ nemu/src/cpu/exec/arith/push.h nemu/src/cpu/exec/arith/test.h \
+ nemu/src/cpu/exec/arith/je.h nemu/src/cpu/exec/arith/cmp.h \
+ nemu/src/cpu/exec/arith/pop.h nemu/src/cpu/exec/arith/ret.h \
+ nemu/src/cpu/exec/arith/sub.h nemu/src/cpu/exec/arith/jmp.h \
+ nemu/src/cpu/exec/arith/jbe.h nemu/src/cpu/exec/arith/adc.h \
+ nemu/src/cpu/exec/arith/add.h nemu/src/cpu/exec/arith/sbb.h \
+ nemu/src/cpu/exec/arith/setne.h nemu/src/cpu/exec/arith/jne.h \
+ nemu/src/cpu/exec/arith/jle.h nemu/src/cpu/exec/arith/jg.h \
+ nemu/src/cpu/exec/arith/jl.h nemu/src/cpu/exec/arith/sete.h \
+ nemu/src/cpu/exec/arith/jge.h nemu/src/cpu/exec/arith/ja.h \
+ nemu/src/cpu/exec/arith/js.h nemu/src/cpu/exec/arith/jns.h \
+ nemu/src/cpu/exec/arith/jae.h nemu/src/cpu/exec/arith/jb.h \
+ nemu/src/cpu/exec/logic/and.h nemu/src/cpu/exec/logic/or.h \
+ nemu/src/cpu/exec/logic/not.h nemu/src/cpu/exec/logic/xor.h \
+ nemu/src/cpu/exec/logic/sar.h nemu/src/cpu/exec/logic/shl.h \
+ nemu/src/cpu/exec/logic/shr.h nemu/src/cpu/exec/logic/shrd.h \
+ nemu/src/cpu/exec/string/rep.h nemu/src/cpu/exec/string/movs.h \
+ nemu/src/cpu/exec/string/lods.h nemu/src/cpu/exec/string/stos.h \
+ nemu/src/cpu/exec/string/scas.h nemu/src/cpu/exec/misc/misc.h \
+ nemu/src/cpu/exec/special/special.h
